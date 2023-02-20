ee669a76c830078d402343d094ea03b30253ab8c
danskis <daniilsn02@gmail.com>
2023-02-20 19:46:45 +0100

    scale

--- BabelProject/src/app/services/filters/filter_scale.service.ts
+++ BabelProject/src/app/services/filters/filter_scale.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from "@angular/core";
 
 export interface DataFilterScale {
-    scale: number;
-    imgPath: string;
-    id: number;
+    scale?: number;
+    imgPath?: string;
+    id?: number;
+    context?:string;
 }
 
 @Injectable({providedIn: 'root'})
@@ -11,6 +12,7 @@ export class FilterScaleService{
 
     private _scale : DataFilterScale[] = [
         {scale: 12, imgPath: '../../../assets/img/filters/byScale/megamenu_1_12.png', id: 1},
+        {context: 'In the 1:18 scale, one centimeter of the miniature represents 18 centimeters of the real model, or what is the same; 18 centimeters of the real model are represented in 1 centimeter of the miniature. To know the measurements of a 1:18 scale model, we will have to divide the measurements of the real model by 18, and so on for each of the different scales.'},
         {scale: 18, imgPath: '../../../assets/img/filters/byScale/megamenu_1_18.png', id: 2},
         {scale: 24, imgPath: '../../../assets/img/filters/byScale/megamenu_1_24.png', id: 3},
         {scale: 43, imgPath: '../../../assets/img/filters/byScale/megamenu_1_43.png', id: 4},
